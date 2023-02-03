811e791abe935e007636baf468c5f71b7a4b3742
SUPAKORN-THV <122855260+SUPAKORN-THV@users.noreply.github.com>
2023-02-03 10:15:40 +0000

    main_V1

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
-#define N 5
 #include <stdio.h>
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={1,6,5,2,3};
+int main(int argc, char *argv[]) {
+  int i;
+  int N = argc - 2; // assign arguments count - 2 to N as Number to run
+  
+  int *a = (int *)malloc(sizeof(int) * N);
+  for (i = 0; i < N; i++){
+    a[i] = atoi(argv[i + 2]); // Change arguments to int
+  }
+  
+  //printf("Count is %d\n and text is %s",N,argv[1]);
+
+  
+  /*int a[N]={1,6,5,2,3};
   int i,j,new_number;
 
    display(a,N);
@@ -11,7 +22,7 @@ int main() {
    insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
+   display(a,N);*/
  return 0;
 }
 
